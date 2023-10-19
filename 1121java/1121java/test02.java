f24e194224674fce0c12bbf6ab6cbddc5b612aeb
AndyXZP <113650347+andy0119@users.noreply.github.com>
2023-10-19 17:04:10 +0800

    Delete 1121java/1121java directory

--- 1121java/1121java/test02.java
+++
@@ -1,8 +0,0 @@
-public class test02{
-    public static void main(String[]args){
-        float a=2,b=5;
-        float sum = a/b;
-        
-        System.out.println(a + "÷" + b + "=" + sum ); 
-    }
-}
